f9c806efe7ca012e862df4e9fb1b0810c9443286
Adel Hasic <47243901+hasicx@users.noreply.github.com>
2019-11-22 17:37:22 +0100

    Add files via upload
    v2

--- Term.java
+++ Term.java
@@ -6,11 +6,11 @@ import java.util.Comparator;
 public class Term //implements Comparator
 {
 	private String query;
-	private int weight;
+	private double weight;
 
 	// Initializes a term with the given query string and weight.
-	public Term(String query, int weight)
-	{ 
+	public Term(String query, double weight)
+	{
 		try
 		{
 			if(query == null)
@@ -22,10 +22,10 @@ public class Term //implements Comparator
 			}
                         if(weight < 0)
                         {
-                                java.lang.IllegalArgumentException negative =
+                                java.lang.IllegalArgumentException negativeWeight =
                                         new java.lang.IllegalArgumentException("Numeric"
                                         + " argument (weight) cannot be negative.");
-                                throw negative;
+                                throw negativeWeight;
 
                         }
 		} 
@@ -35,34 +35,64 @@ public class Term //implements Comparator
 			System.out.println(nullString.getMessage());
 			return;
 		}
-		catch(IllegalArgumentException negative)
+		catch(IllegalArgumentException negativeWeight)
 		{
-			System.out.println(negative.getMessage());
+			System.out.println(negativeWeight.getMessage());
 			return;
 		}
-		
 
 		this.query = query;
-		this.weight = weight;		
+		this.weight = weight;
 	}
 	public String getQuery()
 	{
 		return this.query;
 	}
-	public int getWeight()
+	public double getWeight()
 	{
 		return this.weight;
 	}
-/*
+
 	// Compares the two terms in lexicographic order by query.
-	public static Comparator<Term> byLexicographicOrder() {}
+	public static Comparator<Term> byLexicographicOrder()
+	{
+		Comparator<Term> comp = new byLexicographicOrder();
+		return comp;
+	}
+
 
 	// Compares the two terms in descending order by weight.
-	public static Comparator<Term> byReverseWeightOrder() {}
+	public static Comparator<Term> byReverseWeightOrder()
+	{
+		Comparator<Term> comp = new byReverseWeightOrder();
+		return comp;
+	}
+
+
+
 
 	// Compares the two terms in lexicographic order,
 	// but using only the first k characters of each query.
-	public static Comparator<Term> byPrefixOrder(int k) {}    */
+/*
+	public static Comparator<Term> byPrefixOrder(int k)
+	{
+		try
+                {
+                        if(k < 0)
+                        {
+                                java.lang.IllegalArgumentException negativeArg =
+                                        new java.lang.IllegalArgumentException("Numeric"
+                                        + " argument (weight) cannot be negative.");
+                                throw negativeArg;
+                        }
+                }
+
+                catch(IllegalArgumentException negativeArg)
+                {
+                        System.out.println(negativeArg.getMessage());
+                        return;
+                }
+	} */
 
 	// Returns a string representation of this term in the following format:
 	// the weight, followed by whitespace, followed by the query.
@@ -71,4 +101,24 @@ public class Term //implements Comparator
 		return String.format("%12d    %s", this.weight, this. query);
 	}
 
+	private static class byLexicographicOrder implements Comparator<Term>
+	{
+		public int compare(Term t1, Term t2)
+		{
+			return t1.getQuery().compareTo(t2.getQuery());
+		}
+	}
+
+	private static class byReverseWeightOrder implements Comparator<Term>
+	{
+		public int compare(Term t1, Term t2)
+		{
+			Double dbl1 = t1.getWeight();
+			Double dbl2 = t2.getWeight();
+
+			return -dbl1.compareTo(dbl2);
+		}
+	}
+
 }
+
